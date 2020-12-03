65b32b15eef85a88b594255b02294dcae422063e
Pablo <pablopezgarcia97@gmail.com>
2020-12-03 19:15:28 +0100

    repasando imagenes glid y efectos scrim

--- app/src/main/java/com/dam/mynicestart/MainActivity.java
+++ app/src/main/java/com/dam/mynicestart/MainActivity.java
@@ -2,7 +2,9 @@ package com.dam.mynicestart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void pasarAmain2(View view) {
+
+        Intent i = new Intent(this, MainActivity2.class);
+
+        startActivity(i);
+
     }
 }
--- app/src/main/java/com/dam/mynicestart/SplashActivity.java
+++ app/src/main/java/com/dam/mynicestart/SplashActivity.java
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,6 +18,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ImageView myLogo = (ImageView) findViewById(R.id.imageView2);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        myLogo.startAnimation(myanim);
+
+        TextView myTitulo = (TextView) findViewById(R.id.titulo);
+        Animation myanim2 = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        myTitulo.startAnimation(myanim);
+
+        ImageView cityskyline = findViewById(R.id.cityskyline);
+
+        Glide.with(this).load(R.drawable.cityskyline).centerCrop().into(cityskyline);
 
         openApp(true);
 
